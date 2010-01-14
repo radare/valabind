5aaa802850ed7f789edd7e2486f22e7802887f13
pancake <pancake@nopcode.org>
2010-01-14 18:39:17 +0100

    * Drop bashism in valaswig-cc

--- t/r2/test.java
+++ t/r2/test.java
@@ -1,8 +1,11 @@
+//import java.math;
+
 class test {
 	public static void main (String[] args) {
+		System.loadLibrary ("r_bp");
 		rBreakpoint bp = new rBreakpoint ();
 		bp.use ("x86");
-		bp.add (0x804800, 0, 0);
+		bp.add_sw (new java.math.BigInteger("0x804800"), 0, 0);
 		bp.list (0);
 	}
 }
